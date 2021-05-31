9ba4c9f407d05d8988e4392a7ab655f4c3c02c0d
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-31 20:24:46 -0300

    Aula 'useCallback' (C5-M1-SM2)

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -8,9 +8,10 @@ interface SearchResultsProps {
     price: number
     title: string
   }>
+  onAddToWishlist: (id: number) => Promise<void>
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
+export function SearchResults({ results, onAddToWishlist }: SearchResultsProps) {
   const totalPrice = useMemo(() => {
     return results.reduce((total, product) => {
       return total + product.price
@@ -22,7 +23,11 @@ export function SearchResults({ results }: SearchResultsProps) {
       <h2>{totalPrice}</h2>
       
       {results.map(product => (
-        <ProductItem key={product.id} product={product} />
+        <ProductItem
+          key={product.id}
+          product={product}
+          onAddToWishlist={onAddToWishlist}
+        />
       ))}
     </div>
   )
